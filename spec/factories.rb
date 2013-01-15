dba6244e1111cbf34995f1bcdfe16530a664ad5d
ARCANGIOLI Robin <arcangioli.robin@etu.unice.fr>
2013-01-14 16:15:14 -0800

    Finish user edit, update, index, and destroy actions.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    name      "Hartl"
-    firstname "Michael"
-    email     "michael@example.com"
-    password  "foobar"
+    sequence(:name)  { |n| "name_#{n}" }
+    sequence(:firstname)  { |n| "firstname_#{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
+    password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
